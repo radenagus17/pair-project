0ef0a1f33abbe8bfe18e1ddacf747403a995c921
radenagus17_ <vaghuz@gmail.com>
2022-07-12 14:11:20 +0800

    ready to deploy

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-const express = require("express");
-const router = require("./routes");
-const session = require('express-session');
-const bcrypt = require('bcryptjs');
-const app = express();
-const port = 3000;
+"use strict";
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: "supercalifragilisticexpialidocious",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 60000
-    }
-  }));
-app.use("/", router);
+const app = require("./express/server");
 
-app.listen(port, () => console.log(`I Love yaa... ${port}`));
+app.listen(3000, () => console.log("Local app listening on port 3000!"));
